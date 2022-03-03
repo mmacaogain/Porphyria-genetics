a840092afca56476f3143c40f5267e94a3d788c1
mmacaogain <m.macaogain@gmail.com>
2022-03-03 21:35:28 +0000

    Found and deleted an odd symbol in the database - Fixed searchbox error

--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 library(DT)
 #library(shiny)
-
+#PorData<-read.csv("Scores.csv",encoding="UTF-8")
+#PorData<-readr::read_csv("scores.csv")
 PorData<-read.csv("scores.csv", row.names = 1)
 ui<-basicPage(
   h2("PorphyriaDB - Biochemical Genetics Laboratory, St. James's Hospital "),
